2969794c91307992dfac82c1e7ec0aebfabf75e1
Flávio Martins da Silva <flaviomartins_silva@hotmail.com>
2019-09-24 13:41:20 -0300

    fixed/Add more attributes in index return

--- src/app/controllers/OrganizingController.js
+++ src/app/controllers/OrganizingController.js
@@ -1,9 +1,22 @@
 import Meetup from '../models/Meetup';
+import User from '../models/User';
+import File from '../models/File';
 
 class OrganizingController {
   async index(req, res) {
-    const meetups = await Meetup.findAll({ where: { user_id: req.userId } });
-
+    const meetups = await Meetup.findAll({ 
+      where: { user_id: req.userId }, 
+      include: [
+        {
+          model: User,
+          attributes: ['id', 'name', 'email'],
+        },
+        {
+          model: File,
+          attributes: ['id', 'path', 'url'],
+        },
+      ], 
+    });
     return res.json(meetups);
   }
 }
